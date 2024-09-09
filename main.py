3dcb6a7ebdefc4c2ccdd936ea5bb59c35b08d1c3
kundup <kundup61@hotmail.com>
2024-09-08 22:40:37 -0700

    collision detection done, bpundary done, score addition done

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame as pg
 import random
 
-import pygame.key
-
 pg.init()
 
 # initializing
-screen = pg.display.set_mode((500, 500))
+width = 500
+height = 500
+screen = pg.display.set_mode((width, height))
 pg.display.set_caption("snake_game")
 clock = pg.time.Clock()
 FPS = 60
+game_over = False
 
 
 # Bait class
@@ -18,15 +19,22 @@ class Bait(pg.sprite.Sprite):
         super().__init__()
         self.image = pg.image.load("coin.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(50, 400)
-        self.rect.y = random.randint(50, 400)
+        self.rect.x = random.randint(50, 200)
+        self.rect.y = random.randint(50, 200)
+        self.score = 0
 
     def update(self):
+        if self.rect.colliderect(player.rect):
+            self.score += 1
+            self.rect.x = random.randint(10, 480)
+            self.rect.y = random.randint(10, 480)
+
         screen.blit(self.image, self.rect)
 
 
-class Player():
+class Player(pg.sprite.Sprite):
     def __init__(self, x, y):
+        super().__init__()
         img = pg.image.load("block.jpg").convert_alpha()
         self.image = pg.transform.smoothscale(img, (15, 15))
         self.rect = self.image.get_rect()
@@ -36,30 +44,35 @@ class Player():
         self.dx = self.direction
         self.dy = 0
 
-    def update(self):
+    def update(self, game_over):
 
-        pressed_key = pg.key.get_pressed()
-        if pressed_key[pg.K_DOWN]:
-            self.dx = 0 * self.direction
-            self.dy = 1 * self.direction
+        if not game_over:
 
-        elif pressed_key[pg.K_LEFT]:
-            self.dy = 0
-            self.dx = -1 * self.direction
+            pressed_key = pg.key.get_pressed()
+            if pressed_key[pg.K_DOWN]:
+                self.dx = 0 * self.direction
+                self.dy = 1 * self.direction
 
-        elif pressed_key[pg.K_RIGHT]:
-            self.dy = 0
-            self.dx = self.direction
+            elif pressed_key[pg.K_LEFT]:
+                self.dy = 0
+                self.dx = -1 * self.direction
 
-        elif pressed_key[pg.K_UP]:
-            self.dy = -1 * self.direction
-            self.dx = 0 * self.direction
+            elif pressed_key[pg.K_RIGHT]:
+                self.dy = 0
+                self.dx = self.direction
 
+            elif pressed_key[pg.K_UP]:
+                self.dy = -1 * self.direction
+                self.dx = 0 * self.direction
 
-        self.rect.x += self.dx
-        self.rect.y += self.dy
+            self.rect.x += self.dx
+            self.rect.y += self.dy
+
+            if self.rect.y <= 0 or self.rect.bottom >= height or self.rect.x <= 0 or self.rect.right >= width:
+                game_over: bool = True
 
         screen.blit(self.image, self.rect)
+        return game_over
 
 
 bait = Bait()
@@ -72,10 +85,20 @@ while run:
         if event.type == pg.QUIT:
             run = False
 
-    bait.update()
-    player.update()
+    game_over = player.update(game_over)
+
+    if not game_over:
+        bait.update()
 
     pg.display.update()
     clock.tick(FPS)
 
 pg.quit()
+
+# collision (done)
+# boundary (done)
+# score addition
+# opening and game_over screen
+# acceleration
+# enlarging size
+# high score saving
